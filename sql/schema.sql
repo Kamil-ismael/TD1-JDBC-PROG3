5c0fd62774688d509cda0fdd1016b3b3f83bc8e0
Ismael <hei.ismael.3@gmail.com>
2025-12-08 20:35:47 +0100

    add file schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+-- Table Product
+CREATE TABLE Product (
+    Product_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL?
+    creation_datetime TIMESTAMP NOT NULL DEFAULT Now()
+);
+
+-- Table Product_category
+CREATE TABLE Product_category (
+    Prouct_category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    product_id INT NOT NULL,
+    CONSTRAINT fk_product
+        FOREIGN KEY (product_id)
+        REFERENCES Product(Product_id)
+        ON DELETE CASCADE
+);
+
+-- Droit à l'utilisateur
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO product_manager_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO product_manager_user;
