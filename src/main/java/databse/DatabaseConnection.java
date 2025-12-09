60736a96ba68f3e6f8e2637efb0decafd4224581
Ismael <hei.ismael.3@gmail.com>
2025-12-09 16:16:02 +0100

    update database connection

--- src/main/java/databse/DatabaseConnection.java
+++ src/main/java/databse/DatabaseConnection.java
@@ -1,37 +1,25 @@
-package databse;
+package databse ;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseConnection {
     private final String url;
     private final String user;
     private final String password;
-    private Connection conn;
 
     public DatabaseConnection(String url, String user, String password, Connection conn) {
         this.url = url;
         this.user = user;
         this.password = password;
-        this.conn = null;
     }
 
-    public Connection getConnection() {
+    public Connection getDBConnection(){
         try {
-            if (conn == null || getConnection().isClosed()) {
-                Class.forName("org.postgresql.Driver");
-
-                conn = DriverManager.getConnection(url, user, password);
-                System.out.println("Connected to PostgreSQL database");
-            }
-        } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver not found : " + e.getMessage());
-        } catch (SQLException e) {
-            System.out.println("Connection fail: " + e.getMessage());
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new RuntimeException("Connection error : " + e.getMessage(),e);
         }
-        return conn;
     }
-
-
 }
