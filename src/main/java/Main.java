14eed6696134ef140172c77e5cc774d5dd4a5835
Ismael <hei.ismael.3@gmail.com>
2025-12-09 17:55:25 +0100

    add file DataRetriever

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import databse.DatabaseConnection;
+import model.Category;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/dao/DataRetriever.java
+++ src/main/java/dao/DataRetriever.java
@@ -1,4 +1,39 @@
 package dao;
 
+import databse.DatabaseConnection;
+import model.Category;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataRetriever {
+    private final DatabaseConnection dbConnection;
+
+    public DataRetriever(DatabaseConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT id, name FROM product_category";
+
+        try (Connection conn = dbConnection.getDBConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Category category = new Category();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            System.out.println("recuperation error : " + e.getMessage());
+        }
+        return categories;
+    }
 }
